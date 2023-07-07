4d8c100a58be872ee775afe310cd0223a1ce241e
chokwatcharathep <chok.watcharathep@maqe.com>
2023-07-07 22:01:43 +0700

    chore(swagger): add api tag

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { CreateFirebaseUserDto } from '../dtos/create-firebase-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
